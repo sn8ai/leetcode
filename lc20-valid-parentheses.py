774c73cbf74cbd890222db83a93452977c86afed
Shane Ngai <n8ai@icloud.com>
2024-01-19 21:16:02 -0800

    Complete LC #20

---
+++ lc20-valid-parentheses.py
@@ -0,0 +1,14 @@
+def isValid(s):
+	stack = []
+	parentheses = {")" : "(", "}": "{", "]" : "["}
+
+	for c in s:
+		if c in parentheses:
+			if stack and stack[-1] == parentheses[c]:
+				stack.pop()
+			else:
+				return False
+		else:
+			stack.append(c)
+
+	return True if not stack else False
